23b2f80964d8ef092de86d698f27672654e80473
SemirS23 <116985783+SemirS23@users.noreply.github.com>
2024-05-13 15:25:49 -0500

    footer and homepage

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,11 @@
+import styles from "./footer.module.css";
+
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className={styles.container}>
+            <div className={styles.logo}>SKyTech</div>
+            <div className={styles.logo}>SKyTech © All rights reserved.</div>
+        </div>
     )
 }
 
